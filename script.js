d3e75da093fee66d911e5065cfe55b3033cee865
igor02b1 <igor02b1@gmail.com>
2022-09-08 16:50:52 -0300

    variaveis

---
+++ script.js
@@ -0,0 +1,18 @@
+
+const prompt = require('prompt-sync')();
+
+const userName = prompt('Digite seu nome: ');
+    
+const weight  = prompt('Digite o seu peso: ');
+   
+const height  = prompt('Digite sua altura: ');
+    
+const hospitalization  = prompt('Digite a data da sua hospitalização: ');
+    
+const diabete  = prompt('O paciente é diabético: (Y)sim // (N)não: ');
+
+    
+
+
+
+
